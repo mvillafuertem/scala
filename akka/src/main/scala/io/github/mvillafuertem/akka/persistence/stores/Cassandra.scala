02bf9791461a7fe67db3cb45086d596888930c8f
Miguel Villafuerte <mivimur@hotmail.com>
2019-04-26 19:42:24 +0200

    feature(Persistence) : add persistence cassandra

---
+++ akka/src/main/scala/io/github/mvillafuertem/akka/persistence/stores/Cassandra.scala
@@ -0,0 +1,22 @@
+package io.github.mvillafuertem.akka.persistence.stores
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+object Cassandra extends App {
+
+  implicit val actorSystem: ActorSystem = ActorSystem("Cassandra", ConfigFactory.load().getConfig("cassandra"))
+  val simplePersistentActor = actorSystem.actorOf(Props[SimplePersistentActor], "simplePersistentActor")
+
+  for (i <- 1 to 10) {
+    simplePersistentActor ! s"I love Akka [$i]"
+  }
+
+  simplePersistentActor ! "print"
+  simplePersistentActor ! "snap"
+
+  for (i <- 11 to 20) {
+    simplePersistentActor ! s"I love Akka [$i]"
+  }
+
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,13 @@ object Dependencies {
   val production: Seq[ModuleID] = Seq(
     // Akka Persistence
     "com.typesafe.akka" %% "akka-persistence" % Version.akka,
-    "org.postgresql" % "postgresql" % "42.2.5",
+
+    // Akka Persistence Cassandra
+    "com.typesafe.akka" %% "akka-persistence-cassandra" % Version.akkaPersistenceCassandra,
+
+    // Akka Persistence JDBC
     "com.github.dnvriend" %% "akka-persistence-jdbc" % Version.akkaPersistenceJdbc,
+    "org.postgresql" % "postgresql" % "42.2.5",
 
     // Akka Stream
     "com.typesafe.akka" %% "akka-stream" % Version.akka,
@@ -18,12 +23,15 @@ object Dependencies {
     //"com.typesafe.akka" %% "akka-testkit" % Versions.akka % Test,
     "com.typesafe.akka" %% "akka-testkit" % Version.akka,
     //"com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka % Test
-    "com.typesafe.akka" %% "akka-stream-testkit" % Version.akka
+    "com.typesafe.akka" %% "akka-stream-testkit" % Version.akka,
+    
+    "com.typesafe.akka" %% "akka-persistence-cassandra-launcher" % Version.akkaPersistenceCassandra % Test
   )
 
   object Version {
     val akka = "2.5.22"
-    val akkaPersistenceJdbc = "3.4.0"
+    val akkaPersistenceJdbc = "3.5.0"
+    val akkaPersistenceCassandra = "0.94"
     val scalaTest = "3.0.7"
     val postgres = "42.2.5"
   }
