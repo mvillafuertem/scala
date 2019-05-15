1d2bded58061cc1bd38bb9bc3e8a6eac280eaeb5
Miguel Villafuerte <mivimur@hotmail.com>
2019-05-15 16:50:08 +0200

    chore(Travis) : fix build

---
+++ algorithms/src/main/scala/io/github/mvillafuertem/algorithms/data/structures/tree/TreeElement.scala
@@ -0,0 +1,18 @@
+package io.github.mvillafuertem.algorithms.data.structures.tree
+
+class TreeElement {
+//  nivel = 0
+//  nodo = null
+//  private var nivel = 0
+//  private var nodo = null
+//
+//  def this(a: Nothing, n: Int) {
+//    this()
+//    nivel = n
+//    nodo = a
+//  }
+//
+//  def getNodoArbol: Nothing = nodo
+//
+//  def getNivel: Int = nivel
+}
