5a99431ff3dbc230bc413ae604e4c4a15eab1459
Miguel Villafuerte <mivimur@hotmail.com>
2019-09-02 00:07:30 +0200

    feature(AkkaFSM) : add poc persistent fsm

---
+++ applications/todo/src/test/scala/io/github/mvillafuertem/akka/todo/infrastructure/ToDoReceptionistSpec.scala
@@ -0,0 +1,13 @@
+package io.github.mvillafuertem.akka.todo.infrastructure
+
+import org.scalatest.FlatSpecLike
+
+final class ToDoReceptionistSpec extends FlatSpecLike {
+
+}
+
+object ToDoReceptionistSpec {
+
+
+
+}
