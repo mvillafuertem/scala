b06c50759813c59d556b128c9d5ddbdcbb5b6eac
Miguel Villafuerte <mivimur@hotmail.com>
2019-11-09 19:26:32 +0100

    feature(AkkaTypedPersistence) : integrate with streams

--- akka-typed/src/test/scala/io/github/mvillafuertem/akka/typed/persistent/PersistentActorSpec.scala
+++ akka-typed/src/test/scala/io/github/mvillafuertem/akka/typed/persistent/PersistentActorSpec.scala
@@ -1,15 +1,20 @@
 package io.github.mvillafuertem.akka.typed.persistent
 
 import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.stream.scaladsl.Source
+import akka.stream.typed.javadsl.ActorSink
 import com.typesafe.config.{Config, ConfigFactory}
-import io.github.mvillafuertem.akka.typed.persistent.PersistentActor.{AddPerson, GetPerson, ModifyLastName, Person, PersonAdded, PersonState}
+import io.github.mvillafuertem.akka.typed.persistent.PersistentActor.{AddPerson, GetPerson, ModifyLastName, Person}
 import org.scalatest.{FlatSpecLike, Matchers}
 
+import scala.concurrent.duration._
+
+
 /**
- * @author Miguel Villafuerte
- */
+  * @author Miguel Villafuerte
+  */
 final class PersistentActorSpec extends ScalaTestWithActorTestKit(PersistentActorSpec.conf)
- with FlatSpecLike with Matchers {
+  with FlatSpecLike with Matchers {
 
   behavior of "Persistent Actor"
 
@@ -34,6 +39,34 @@ final class PersistentActorSpec extends ScalaTestWithActorTestKit(PersistentActo
     val person = Person("Pepe", "Pipo")
     val value = spawn(PersistentActor.supervisedBehavior("id"))
     val probe = TestProbe[Person]
+
+    // w h e n
+    value ! AddPerson(person)
+    value ! ModifyLastName("Popu")
+    value ! GetPerson(probe.ref)
+
+    // t h e n
+    probe.expectMessage(Person("Pepe", "Popu"))
+  }
+
+  it should "integrate with streams source person" in {
+    // g i v e n
+    val person = Person("Pepe", "Pipo")
+    val value = spawn(PersistentActor.supervisedBehavior("id"))
+    val probe = TestProbe[Person]
+
+    val actorSink = ActorSink.actorRef[PersistentActor.Command](value.ref,
+      GetPerson(probe.ref),
+      _ => AddPerson(person))
+
+    Source(1 to 10)
+      .throttle(1, 1 second)
+      .map(n => Person(s"Pepe $n", "Pipo"))
+      .log("source")
+      .map(p => AddPerson(p))
+      .to(actorSink)
+      .run()
+
 
     // w h e n
     value ! AddPerson(person)
