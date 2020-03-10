46f4086b5ef264a7883f3f92eee3a01b27830e79
Scala Steward <me@scala-steward.org>
2020-03-11 00:43:09 +0100

    Update postgresql to 42.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -257,7 +257,7 @@ object Dependencies {
     val dijon = "0.3.0"
     val leveldbjniAll = "1.8"
     val logback = "1.2.3"
-    val postgres = "42.2.9"
+    val postgres = "42.2.11"
     val scalaTest = "3.1.0"
     val slick = "3.3.2"
     val specs2 = "4.8.3"
