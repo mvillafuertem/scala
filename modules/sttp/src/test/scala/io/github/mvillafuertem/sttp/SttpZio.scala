bdea2ccbaaead5b48bca8cc716012517ae4a46db
Miguel Villafuerte <mivimur@hotmail.com>
2020-05-03 12:55:12 +0200

    test(sttp) : use of absolve

--- modules/sttp/src/test/scala/io/github/mvillafuertem/sttp/SttpZio.scala
+++ modules/sttp/src/test/scala/io/github/mvillafuertem/sttp/SttpZio.scala
@@ -16,52 +16,58 @@ object SttpZio extends DefaultRunnableSpec {
 
   case class Response(success: Boolean)
   case class Request(success: Boolean)
-  private val uri           = "https://enbom40wuq5zg.x.pipedream.net/1"
-  private val requestGET    = basicRequest.get(uri"$uri").response(asJson[Response])
-  private val requestPOST   = basicRequest.post(uri"$uri").response(asJson[Response]).body[Request](Request(true))
-  private val requestPUT    = basicRequest.put(uri"$uri").response(asJson[Response]).body[Request](Request(true))
-  private val requestDELETE = basicRequest.delete(uri"$uri").response(asJson[Response])
+  private val uri                    = "https://enbom40wuq5zg.x.pipedream.net/1"
+  private val requestGET             = basicRequest.get(uri"$uri").response(asJson[Response])
+  private val requestPOST            = basicRequest.post(uri"$uri").response(asJson[Response]).body[Request](Request(true))
+  private val requestPUT             = basicRequest.put(uri"$uri").response(asJson[Response]).body[Request](Request(true))
+  private val requestDELETE          = basicRequest.delete(uri"$uri").response(asJson[Response])
+  private val equalToResponseSuccess = equalTo(Response(true))
 
-  override def spec: Spec[TestEnvironment, TestFailure[Throwable], TestSuccess] = suite(getClass.getSimpleName)(
-    testM(s"${requestGET.toCurl}")(
-      assertM(
-        backend
-          .flatMap(implicit backend => requestGET.send())
-          .map(_.body)
-      )(equalTo(Right(Response(true))))
-    ),
-    testM(s"${requestPOST.toCurl}")(
-      assertM(
-        backend
-          .flatMap(implicit backend => requestPOST.send())
-          .map(_.body)
-      )(equalTo(Right(Response(true))))
-    ),
-    testM(s"${requestPUT.toCurl}")(
-      assertM(
-        backend
-          .flatMap(implicit backend => requestPUT.send())
-          .map(_.body)
-      )(equalTo(Right(Response(true))))
-    ),
-    testM(s"${requestDELETE.toCurl}")(
-      assertM(
-        backend
-          .flatMap(implicit backend => requestDELETE.send())
-          .map(_.body)
-      )(equalTo(Right(Response(true))))
-    ),
-    testM(s"${{ requestPOST.toCurl }} ++ ${requestGET.toCurl}")(
-      assertM(
-        backend
-          .flatMap(implicit backend =>
-            for {
-              responsePOST <- requestPOST.send().map(_.body)
-              responseGET  <- requestGET.send().map(_.body) if responsePOST.isRight
-            } yield responseGET
-          )
-      )(equalTo(Right(Response(true))))
+  override def spec: Spec[TestEnvironment, TestFailure[Throwable], TestSuccess] =
+    suite(getClass.getSimpleName)(
+      testM(s"${requestGET.toCurl}")(
+        assertM(
+          backend
+            .flatMap(implicit backend => requestGET.send())
+            .map(_.body)
+            .absolve
+        )(equalToResponseSuccess)
+      ),
+      testM(s"${requestPOST.toCurl}")(
+        assertM(
+          backend
+            .flatMap(implicit backend => requestPOST.send())
+            .map(_.body)
+            .absolve
+        )(equalToResponseSuccess)
+      ),
+      testM(s"${requestPUT.toCurl}")(
+        assertM(
+          backend
+            .flatMap(implicit backend => requestPUT.send())
+            .map(_.body)
+            .absolve
+        )(equalToResponseSuccess)
+      ),
+      testM(s"${requestDELETE.toCurl}")(
+        assertM(
+          backend
+            .flatMap(implicit backend => requestDELETE.send())
+            .map(_.body)
+            .absolve
+        )(equalToResponseSuccess)
+      ),
+      testM(s"${requestPOST.toCurl} ++ ${requestGET.toCurl}")(
+        assertM(
+          backend
+            .flatMap(implicit backend =>
+              for {
+                responsePOST <- requestPOST.send().map(_.body)
+                responseGET  <- requestGET.send().map(_.body).absolve if responsePOST.isRight
+              } yield responseGET
+            )
+        )(equalToResponseSuccess)
+      )
     )
-  )
 
 }
