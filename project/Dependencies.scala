bf9105eab7bf760132c6a5afef3a35fcb988e09a
Miguel Villafuerte <mivimur@hotmail.com>
2020-05-10 15:54:29 +0200

    chore(Dependencies) : update artifacts version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -304,7 +304,7 @@ object Dependencies {
     val testcontainersKafka       = "1.14.1"
     val zio                       = "1.0.0-RC18-2"
     val zioAkkaCluster            = "0.1.15"
-    val zioInteropReactiveStreams = "1.0.3.5-RC6"
+    val zioInteropReactiveStreams = "1.0.3.5-RC7"
     val zioKafka                  = "0.8.0"
   }
 
