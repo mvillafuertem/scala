23918ea68d5ca17d7c820bde4955ee120d1c3a91
Scala Steward <me@scala-steward.org>
2020-05-16 20:25:10 +0200

    Update zio-kafka to 0.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -311,7 +311,7 @@ object Dependencies {
     val zio                       = "1.0.0-RC18-2"
     val zioAkkaCluster            = "0.1.15"
     val zioInteropReactiveStreams = "1.0.3.5-RC7"
-    val zioKafka                  = "0.8.0"
+    val zioKafka                  = "0.9.0"
   }
 
 }
