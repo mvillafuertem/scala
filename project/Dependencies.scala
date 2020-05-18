fcbfb62e7412cc2addd1d47433e17364ece54024
Scala Steward <me@scala-steward.org>
2020-05-18 08:24:24 +0200

    Update zio-interop-reactivestreams to 1.0.3.5-RC8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -310,7 +310,7 @@ object Dependencies {
     val testcontainersKafka       = "1.14.2"
     val zio                       = "1.0.0-RC18-2"
     val zioAkkaCluster            = "0.1.15"
-    val zioInteropReactiveStreams = "1.0.3.5-RC7"
+    val zioInteropReactiveStreams = "1.0.3.5-RC8"
     val zioKafka                  = "0.8.0"
   }
 
