af12f7ce2422cf569bbdcdfc5351743f99ad4eed
Scala Steward <me@scala-steward.org>
2020-06-04 18:45:50 +0200

    Update postgresql to 42.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -310,7 +310,7 @@ object Dependencies {
     val jsoniter                  = "2.2.5"
     val leveldbjniAll             = "1.8"
     val logback                   = "1.2.3"
-    val postgres                  = "42.2.12"
+    val postgres                  = "42.2.13"
     val scalaTest                 = "3.1.2"
     val slick                     = "3.3.2"
     val sttp                      = "2.1.5"
