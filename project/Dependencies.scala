2eb3e161d16b3f6f89221f41925b7cd4e013bb92
Scala Steward <me@scala-steward.org>
2020-06-20 16:29:48 +0200

    Update zio-akka-cluster to 0.1.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -318,7 +318,7 @@ object Dependencies {
     val testcontainers            = "0.37.0"
     val testcontainersKafka       = "1.14.3"
     val zio                       = "1.0.0-RC20"
-    val zioAkkaCluster            = "0.1.17"
+    val zioAkkaCluster            = "0.1.18"
     val zioInteropReactiveStreams = "1.0.3.5-RC10"
     val zioKafka                  = "0.9.0"
   }
