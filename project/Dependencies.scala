b1c35a2e8e8cfe2b4a7ad8065d25045041366bc0
Scala Steward <me@scala-steward.org>
2020-07-08 22:36:22 +0200

    Update testcontainers-scala to 0.38.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -315,7 +315,7 @@ object Dependencies {
     val slick                     = "3.3.2"
     val sttp                      = "2.1.5"
     val tapir                     = "0.15.3"
-    val testcontainers            = "0.37.0"
+    val testcontainers            = "0.38.0"
     val testcontainersKafka       = "1.14.3"
     val zio                       = "1.0.0-RC20"
     val zioAkkaCluster            = "0.1.17"
