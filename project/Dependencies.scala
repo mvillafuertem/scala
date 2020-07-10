a546266f74ed8c26fc22d5ce88b91efd41aaae50
Scala Steward <me@scala-steward.org>
2020-07-10 20:24:57 +0200

    Update jsoniter-scala-core, ... to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -307,7 +307,7 @@ object Dependencies {
     val dijon                     = "0.3.0"
     val h2                        = "1.4.200"
     val jslt                      = "0.1.10"
-    val jsoniter                  = "2.4.4"
+    val jsoniter                  = "2.5.0"
     val leveldbjniAll             = "1.8"
     val logback                   = "1.2.3"
     val postgres                  = "42.2.14"
