3d2306d8b96cc642fc048ba440f95592a5f97318
Miguel Villafuerte <mivimur@hotmail.com>
2020-08-12 16:54:46 +0200

    chore(cicd) : fix build

--- modules/spark/src/test/scala/io/github/mvillafuertem/spark/RDDApplicationSpec.scala
+++ modules/spark/src/test/scala/io/github/mvillafuertem/spark/RDDApplicationSpec.scala
@@ -9,6 +9,7 @@ final class RDDReduceByKeySpec extends AnyFlatSpecLike {
 
   it should "Avg House Price" in {
 
+    // g i v e n
     val conf = new SparkConf().setAppName("avgHousePrice").setMaster("local")
     val sc   = new SparkContext(conf)
 
@@ -16,6 +17,7 @@ final class RDDReduceByKeySpec extends AnyFlatSpecLike {
     val lines         = sc.textFile(resourcesPath.getPath)
     val cleanedLines  = lines.filter(line => !line.contains("Bedrooms"))
 
+    // w h e n
     val housePricePairRDD = cleanedLines.map(line => (line.split(",")(3), (1, line.split(",")(2).toDouble)))
 
     val housePriceTotal = housePricePairRDD.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
@@ -24,6 +26,7 @@ final class RDDReduceByKeySpec extends AnyFlatSpecLike {
 
     val housePriceAvg = housePriceTotal.mapValues(avgCount => avgCount._2 / avgCount._1)
     println("HousePriceAvg: ")
+    // t h e n
     for ((bedroom, avg) <- housePriceAvg.collect()) println(bedroom + " : " + avg)
 
   }
