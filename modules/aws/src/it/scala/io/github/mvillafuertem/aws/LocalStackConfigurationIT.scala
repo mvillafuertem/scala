26837cbc8a0cacc035dbcec61e2212dd1661f422
Miguel Villafuerte <mivimur@hotmail.com>
2020-08-14 21:19:41 +0200

    test(aws) : change name of exposed services

--- modules/aws/src/it/scala/io/github/mvillafuertem/aws/LocalStackConfigurationIT.scala
+++ modules/aws/src/it/scala/io/github/mvillafuertem/aws/LocalStackConfigurationIT.scala
@@ -21,7 +21,7 @@ trait LocalStackConfigurationIT extends AsyncFlatSpecLike with Matchers with Bef
   def dockerInfrastructure(logMessageWaitStrategy: LogMessageWaitStrategy): containers.DockerComposeContainer[_] =
     DockerComposeContainer(
       new File(s"${System.getProperty("user.dir")}/modules/aws/src/it/resources/docker-compose.it.yml"),
-      exposedServices = Seq(ExposedService("localstack", AWS_LOCALSTACK_PORT, 1, logMessageWaitStrategy)),
+      exposedServices = Seq(ExposedService("localstack_1", AWS_LOCALSTACK_PORT, 1, logMessageWaitStrategy)),
       identifier = "docker_infrastructure"
     ).container
 
