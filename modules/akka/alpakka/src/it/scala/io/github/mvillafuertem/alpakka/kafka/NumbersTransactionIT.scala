f8858ce1c569a1c8afc38b32d272045fc2146d47
Miguel Villafuerte <mivimur@hotmail.com>
2020-08-18 22:07:48 +0200

    test(alpakka) : fix kafka integration test

--- modules/akka/alpakka/src/it/scala/io/github/mvillafuertem/alpakka/kafka/NumbersTransactionIT.scala
+++ modules/akka/alpakka/src/it/scala/io/github/mvillafuertem/alpakka/kafka/NumbersTransactionIT.scala
@@ -1,32 +1,25 @@
 package io.github.mvillafuertem.alpakka.kafka
 
-import java.io.File
 import java.nio.charset.StandardCharsets
 
-import akka.actor.ActorSystem
-import akka.event.{ Logging, LoggingAdapter }
 import akka.kafka.ConsumerMessage.PartitionOffset
-import akka.kafka.scaladsl.{ Consumer, Producer, Transactional }
-import akka.kafka.testkit.internal.TestcontainersKafka.Singleton.remainingOrDefault
+import akka.kafka.scaladsl.{ Producer, Transactional }
+import akka.kafka.testkit.internal.TestcontainersKafka.Singleton.createProbe
 import akka.kafka.{ ConsumerMessage, ProducerMessage, Subscriptions }
+import akka.stream.Attributes.LogLevels
 import akka.stream.Supervision.{ Restart, Stop }
-import akka.stream.scaladsl.{ Broadcast, Flow, GraphDSL, Keep, Sink, Source, Zip }
+import akka.stream.scaladsl.{ Broadcast, Flow, GraphDSL, Keep, Source, Zip }
 import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
-import akka.stream.testkit.scaladsl.TestSink
-import akka.stream.{ ActorAttributes, ActorMaterializer, FlowShape }
+import akka.stream.{ ActorAttributes, Attributes, FlowShape }
 import akka.{ Done, NotUsed }
-import com.dimafeng.testcontainers.{ DockerComposeContainer, ExposedService }
 import io.github.mvillafuertem.alpakka.kafka.NumbersTransactionIT.NumbersTransactionConfigurationIT
 import io.github.mvillafuertem.alpakka.kafka.properties.KafkaProducerConfigurationProperties
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.scalatest.{ BeforeAndAfterAll, Ignore }
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.flatspec.{ AnyFlatSpecLike, AsyncFlatSpecLike }
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AsyncFlatSpecLike
 import org.scalatest.matchers.should.Matchers
 import org.testcontainers.containers
-import org.testcontainers.containers.wait.strategy.Wait
 
-import scala.collection.immutable
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, Future }
 
@@ -64,13 +57,8 @@ final class NumbersTransactionIT extends NumbersTransactionConfigurationIT {
       Flow[(ConsumerMessage.TransactionalMessage[String, String], Option[String])].map {
         asset: (ConsumerMessage.TransactionalMessage[String, String], Option[String]) =>
           asset._2 match {
-            case Some(v) =>
-              println("SOY   " + v)
-              ProducerMessage.single(new ProducerRecord(sinkTopic, asset._1.record.key(), v), asset._1.partitionOffset)
-
-            case None    =>
-              println("PEPEPEPEPEPEPE")
-              ProducerMessage.passThrough[String, String, PartitionOffset](asset._1.partitionOffset)
+            case Some(v) => ProducerMessage.single(new ProducerRecord(sinkTopic, asset._1.record.key(), v), asset._1.partitionOffset)
+            case None    => ProducerMessage.passThrough[String, String, PartitionOffset](asset._1.partitionOffset)
           }
 
       }
@@ -105,38 +93,25 @@ final class NumbersTransactionIT extends NumbersTransactionConfigurationIT {
             Stop
         })
 
-    /*    Transactional
-      .source(consumerSettings, Subscriptions.topics(Set(sourceTopic)))
-      .log("Numbers Input")
-      .via(graph)
-      .log("Numbers Output")
-      .toMat(Transactional.sink(producerSettings, KafkaProducerConfigurationProperties().transactionalId))(Keep.both)
-      .run()*/
-
+    // W H E N
     val control = Transactional
-      .source(consumerSettings, Subscriptions.topics(sourceTopic))
-      .map { msg =>
-        ProducerMessage.single(new ProducerRecord(sinkTopic, msg.record.key, msg.record.value), msg.partitionOffset)
-      }
-      .via(Transactional.flow(producerSettings, KafkaProducerConfigurationProperties().transactionalId))
-      .toMat(Sink.ignore)(Keep.left)
+      .source(consumerSettings, Subscriptions.topics(Set(sourceTopic)))
+      .log("Transactional Input")
+      .via(graph)
+      .log("Transactional Output")
+      .toMat(Transactional.sink(producerSettings, KafkaProducerConfigurationProperties().transactionalId))(Keep.left)
+      .addAttributes(Attributes.logLevels(onElement = LogLevels.Info))
       .run()
 
-    // W H E N
-
     // T H E N
-    val probeConsumer = Consumer
-      .plainSource(consumerSettings, Subscriptions.topics(Set(sinkTopic)))
-      .map(r => (r.offset(), r.value()))
-      .map(_._2)
-      .runWith(TestSink.probe)
+    val (controlConsumer, probeConsumer) = createProbe(consumerSettings, sinkTopic)
 
     probeConsumer
       .request(10)
-      .expectNextN(Seq("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+      .expectNextN((1 to 10).filterNot(_ == 8).map(_.toString))
 
-    probeConsumer.cancel()
-
+    probeConsumer.expectNoMessage(1.seconds)
+    controlConsumer.shutdown().map(_ shouldBe Done)
     control.shutdown().map(_ shouldBe Done)
 
   }
@@ -160,7 +135,7 @@ object NumbersTransactionIT {
     def produce(): Future[Done] =
       //val _: clients.producer.Producer[String, String] = producerSettings.createKafkaProducer()
       Source(LazyList.from(1 to 10))
-        .throttle(1, 1 milliseconds)
+        .throttle(1, 1 millisecond)
         .log("producer")
         .map(_.toString)
         .map { n =>
