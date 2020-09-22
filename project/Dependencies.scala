597a6286af2c6dbe2adaf8bbe8eb6740f2ea7e2a
Scala Steward <me@scala-steward.org>
2020-09-22 14:38:27 +0200

    Update client:akka-http-backend, ... to 2.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -369,7 +369,7 @@ object Dependencies {
     val slick                     = "3.3.3"
     val slinky                    = "0.6.5"
     val spark                     = "3.0.1"
-    val sttp                      = "2.2.8"
+    val sttp                      = "2.2.9"
     val tapir                     = "0.16.16"
     val testcontainers            = "0.38.3"
     val testcontainersKafka       = "1.14.3"
