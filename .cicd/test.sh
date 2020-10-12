211ce89fc17dad7d7c3b803b1f1639c1a3be2513
Miguel Villafuerte <mivimur@hotmail.com>
2020-10-12 22:10:52 +0200

    chore(Script) : exec test

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -8,6 +8,8 @@ set -o xtrace
 BASE="$(cd "$(dirname "${0}")" && pwd)"
 source ${BASE}/../.env
 
-amm `pwd`/modules/script/SQSConsumerSpec.sc
+sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/2.2.0/2.13-2.2.0) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' \
+&& amm `pwd`/modules/script/SQSConsumerSpec.sc
+
 sbt clean coverage test dependencyUpdates coverageReport
 #sbt clean test dependencyUpdates coverageAggregate
