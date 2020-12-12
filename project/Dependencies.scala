ebb87ab57162b7ad9bfdb0e7147cc39a2772bf42
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-12 16:39:48 +0100

    chore(Dependencies) : update kafka to 1.15.1 (#287)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -412,7 +412,7 @@ object Dependencies {
     val sttp                      = "2.2.9"
     val tapir                     = "0.16.16"
     val testcontainers            = "0.38.7"
-    val testcontainersKafka       = "1.15.0"
+    val testcontainersKafka       = "1.15.1"
     val zio                       = "1.0.3"
     val zioAkkaCluster            = "0.2.0"
     val zioInteropReactiveStreams = "1.3.0.7-2"
