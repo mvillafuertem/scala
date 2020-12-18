f3ee5a79fbabde589a73022dbf52ae5d4e84478f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-18 17:20:44 +0100

    chore(Dependencies) : update testcontainers-scala-core to 0.38.8 (#303)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -433,7 +433,7 @@ object Dependencies {
     val spark                     = "3.0.1"
     val sttp                      = "2.2.9"
     val tapir                     = "0.17.0"
-    val testcontainers            = "0.38.7"
+    val testcontainers            = "0.38.8"
     val testcontainersKafka       = "1.15.1"
     val zio                       = "1.0.3"
     val zioAkkaCluster            = "0.2.0"
