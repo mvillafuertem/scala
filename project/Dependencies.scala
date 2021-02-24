a5e1ba63024ad3fe65c4e6d4850f04f556c1b0f9
Scala Steward <me@scala-steward.org>
2021-02-24 16:39:28 +0100

    chore(Dependencies) : update akka-http-testkit from 10.2.3 to 10.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -412,7 +412,7 @@ object Dependencies {
 
   private object Version {
     val akka                      = "2.6.12"
-    val akkaHttp                  = "10.2.3"
+    val akkaHttp                  = "10.2.4"
     val akkaHttpCirce             = "1.35.3"
     val akkaPersistenceCassandra  = "0.100"
     val akkaPersistenceInmemory   = "2.5.15.2"
