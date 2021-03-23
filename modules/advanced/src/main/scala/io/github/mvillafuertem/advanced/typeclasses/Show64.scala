a6f221cbff96ef609bd8f165ad637122edc2ee6a
Miguel Villafuerte <mivimur@hotmail.com>
2021-03-23 22:23:21 +0100

    refactor(advanced) : use of context abstraction instead of implicit syntax

--- modules/advanced/src/main/scala/io/github/mvillafuertem/advanced/typeclasses/Show64.scala
+++ modules/advanced/src/main/scala/io/github/mvillafuertem/advanced/typeclasses/Show64.scala
@@ -19,7 +19,11 @@ object Show64 {
    *      sin hacer uso de `new`, es muy util si lo juntamos con
    *      implicitos, mirar los test
    */
-  def apply[A](implicit sh: Show64[A]): Show64[A] = sh
+  //def apply[A](implicit sh: Show64[A]): Show64[A] = sh
+  //
+  // this way [A: Show64] is called "context abstraction", means
+  // that the compiler needs to find a semigroup of T in scope
+  def apply[A: Show64]: Show64[A] = implicitly[Show64[A]]
 
   implicit class ShowOps[A: Show64](a: A) {
     def encode: String = Show64[A].encode(a)
@@ -28,6 +32,7 @@ object Show64 {
   implicit val deviceInterpreteShow64: Show64[Device] =
     (a: Device) => new String(Base64.getEncoder.encode(a.toString.getBytes))
 
-  def encode[A: Show64](a: A)(interprete: Show64[A]): String = interprete.encode(a)
+  //def encode[A](a: A)(interprete: Show64[A]): String = interprete.encode(a)
+  def encode[A: Show64](a: A): String = implicitly[Show64[A]].encode(a)
 
 }
