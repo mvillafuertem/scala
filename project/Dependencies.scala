ec7a73a34c22c1d695a82f7b88f56a25f9771b4a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-14 05:47:46 +0200

    chore(Dependencies) : update scala-parser-combinators from 1.1.2 to 2.0.0 (#559)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   val advanced: Seq[ModuleID] = Seq(
     // A D V A N C E D  T E S T
     Artifact.scalaTest        % Version.scalaTest          % Test,
-    "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "2.0.0"
   )
 
   val algorithms: Seq[ModuleID] = Seq(
