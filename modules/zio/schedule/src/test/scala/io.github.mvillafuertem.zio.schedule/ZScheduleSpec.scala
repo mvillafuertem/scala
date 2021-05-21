94a5747c07fe796681743ea00215f2f8aa0cb782
Miguel Villafuerte <mivimur@hotmail.com>
2021-05-21 19:53:05 +0200

    fix(zio) : remove TestAspect.ignore

--- modules/zio/schedule/src/test/scala/io.github.mvillafuertem.zio.schedule/ZScheduleSpec.scala
+++ modules/zio/schedule/src/test/scala/io.github.mvillafuertem.zio.schedule/ZScheduleSpec.scala
@@ -1,44 +1,67 @@
 package io.github.mvillafuertem.zio.schedule
 
-import zio.console.putStrLn
+import zio.Schedule
+import zio.console.putStr
 import zio.duration._
-import zio.test.Assertion.{ equalTo, isUnit }
+import zio.test.Assertion.equalTo
 import zio.test._
-import zio.test.environment.{ TestClock, TestEnvironment }
-import zio.{ Schedule, Task, UIO, ZIO }
+import zio.test.environment.{ TestClock, TestConsole, TestEnvironment }
 
 object ZScheduleSpec extends DefaultRunnableSpec {
-
-  val policy1: Schedule[Any, Any, Duration] =
-    Schedule.exponential(10.milliseconds).tapOutput(o => UIO(println(o))) >>>
-      Schedule.recurWhile(_ < 2.seconds)
-
-  val policy2: Schedule[Any, Any, (Long, Long)] =
-    Schedule.recurs(5) ||
-      Schedule.recurs(10)
 
   override def spec: ZSpec[TestEnvironment, Any] =
     suite(getClass.getSimpleName)(
       testM("track a message")(
         assertM(
           for {
-            _ <- putStrLn("Start")
-            _ <- putStrLn("Initial Delay").delay(5.seconds).fork
-            _ <- TestClock.adjust(5.seconds)
-            _ <- putStrLn("Repeated Delay").repeat(Schedule.spaced(1.second))
-          } yield ()
-        )(isUnit)
+            _                  <- putStr("Start")
+            fiberInitialDelay  <- putStr("Initial Delay")
+                                    .delay(5.seconds) // Delay the print message
+                                    .fork  // Fork is only necessary for testing
+            _                  <- TestClock.adjust(5.seconds)
+            fiberRepeatedDelay <- putStr("Repeated Delay")
+                                    .repeat(
+                                      Schedule.spaced(1.second) >>>
+                                        Schedule.recurWhile(_ < 2)
+                                    ) // Print the message every 1 second until twice
+                                    .fork // Fork is only necessary for testing
+            _                  <- TestClock.adjust(2.seconds)
+            _                  <- fiberInitialDelay.join // This is not necessary
+            _                  <- fiberRepeatedDelay.join // This is not necessary
+            actual             <- TestConsole.output
+          } yield actual
+        )(equalTo(Vector("Start", "Initial Delay", "Repeated Delay", "Repeated Delay", "Repeated Delay")))
       ),
-      testM("repeat a message")(
+      testM("repeat a message using Duration")(
         assertM(
           for {
-            _ <- putStrLn("repeated message")
-                   .repeat(policy1)
-                   .fork
-            _ <- TestClock.adjust(2.seconds)
-          } yield ()
-        )(isUnit)
-      )
+            fiberRepeatedMessage <- putStr("repeated message")
+                                      .repeat(
+                                        Schedule
+                                          .exponential(10.milliseconds)
+                                          .tapOutput(d => putStr(d.toString)) >>>
+                                          Schedule
+                                            .recurWhile(_ < 20.milliseconds)
+                                      )
+                                      .fork
+            _                    <- TestClock.adjust(2.seconds)
+            _                    <- fiberRepeatedMessage.join
+            actual               <- TestConsole.output
+          } yield actual
+        )(equalTo(Vector("repeated message", "PT0.01S", "repeated message", "PT0.02S")))
+      ),
+      testM("repeat a message using recurs") {
+        assertM(
+          for {
+            _      <- putStr("any stuff")
+                        .repeat(
+                          Schedule.recurs(6) >>>
+                            Schedule.recurWhile(_ < 3) // Print the message N times until 3
+                        )
+            actual <- TestConsole.output
+          } yield actual
+        )(equalTo(Vector("any stuff", "any stuff", "any stuff", "any stuff")))
+      }
 //      testM("Schedule.unwrap should return a Schedule out of an effect")(
 //        assertM(
 //          for {
@@ -46,5 +69,5 @@ object ZScheduleSpec extends DefaultRunnableSpec {
 //          } yield Schedule.unwrap(effect)
 //        )(equalTo())
 //      )
-    ) @@ zio.test.TestAspect.ignore
+    ) //@@ zio.test.TestAspect.ignore
 }
