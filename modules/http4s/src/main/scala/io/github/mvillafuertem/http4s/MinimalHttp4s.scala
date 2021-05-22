afe1d76fded655918dd7acfa97c5cf6bc753b1e0
Miguel Villafuerte <mivimur@hotmail.com>
2021-05-22 12:51:51 +0200

    chore(Dependencies) : update http4s-blaze-server, http4s-dsl from 1.0.0-M21 to 1.0.0-M22

--- modules/http4s/src/main/scala/io/github/mvillafuertem/http4s/MinimalHttp4s.scala
+++ modules/http4s/src/main/scala/io/github/mvillafuertem/http4s/MinimalHttp4s.scala
@@ -5,7 +5,7 @@ import org.http4s.{ HttpRoutes, Request, Response }
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
 import org.http4s.server.Server
-import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.blaze.server.BlazeServerBuilder
 import zio._
 import zio.console.Console
 import zio.interop.catz._
