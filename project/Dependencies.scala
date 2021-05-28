d4b645e70558ffc24b3156725eefb324e7bbb463
Scala Steward <me@scala-steward.org>
2021-05-28 13:00:54 +0200

    chore(Dependencies) : update spark-core, spark-sql, ... from 3.1.1 to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -457,7 +457,7 @@ object Dependencies {
     val scalaTest                 = "3.2.8"
     val slick                     = "3.3.3"
     val slinky                    = "0.6.6"
-    val spark                     = "3.1.1"
+    val spark                     = "3.1.2"
     val sttp                      = "3.3.4"
     val tapir                     = "0.17.19"
     val testcontainers            = "0.39.4"
