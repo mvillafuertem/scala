ffb7a907f6e0e4b974c0b5624abf9cd202bb6dd7
Scala Steward <me@scala-steward.org>
2021-06-02 08:42:42 +0200

    chore(Dependencies) : update zio, zio-streams, zio-test, ... from 1.0.7 to 1.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -462,7 +462,7 @@ object Dependencies {
     val tapir                     = "0.17.19"
     val testcontainers            = "0.39.5"
     val testcontainersKafka       = "1.15.3"
-    val zio                       = "1.0.7"
+    val zio                       = "1.0.9"
     val zioAkkaCluster            = "0.2.0"
     val zioInteropCats            = "3.1.1.0"
     val zioInteropReactiveStreams = "1.3.5"
