25abc73ab55802eacdf9b77c2c042af8a629df1c
Scala Steward <me@scala-steward.org>
2021-08-05 20:45:12 +0200

    chore(Dependencies) : update akka-actor-testkit-typed, ... from 2.6.14 to 2.6.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -439,7 +439,7 @@ object Dependencies {
   }
 
   private object Version {
-    val akka                      = "2.6.14"
+    val akka                      = "2.6.15"
     val akkaHttp                  = "10.2.6"
     val akkaHttpCirce             = "1.37.0"
     val akkaPersistenceCassandra  = "0.100"
