fc9e1c695e9ade798e0aac04b2f5cdf1892232fb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-29 06:46:43 +0200

    chore(Dependencies) : update scala-java8-compat from 1.0.0 to 1.0.1 (#775)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -459,7 +459,7 @@ object Dependencies {
     val dijon                     = "0.5.0"
     val ficus                     = "1.5.1"
     val h2                        = "1.4.200"
-    val java8Compat               = "1.0.0"
+    val java8Compat               = "1.0.1"
     val jslt                      = "0.1.11"
     val jsoniter                  = "2.10.2"
     val jwtCirce                  = "4.3.0"
