810028ad2292860fff24847bf93d9fa7be638209
Scala Steward <me@scala-steward.org>
2021-10-27 14:25:40 +0200

    chore(Dependencies) : update jsoniter-scala-core, ... from 2.11.0 to 2.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -461,7 +461,7 @@ object Dependencies {
     val h2                        = "1.4.200"
     val java8Compat               = "1.0.1"
     val jslt                      = "0.1.11"
-    val jsoniter                  = "2.11.0"
+    val jsoniter                  = "2.11.1"
     val jwtCirce                  = "4.3.0"
     val kafka                     = "2.8.0"
     val leveldbjniAll             = "1.8"
