dcc0f1321570b4f1007888b2cd62d18e3f251e7a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-28 05:19:25 +0100

    chore(Dependencies) : update cats-core, cats-free from 2.6.1 to 2.7.0 (#878)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -452,7 +452,7 @@ object Dependencies {
     val awsLambda                 = "1.2.1"
     val awsSdk                    = "2.17.90"
     val cask                      = "0.7.11"
-    val cats                      = "2.6.1"
+    val cats                      = "2.7.0"
     val circe                     = "0.13.0"
     val http4s                    = "1.0.0-M29"
     val curator                   = "5.2.0"
