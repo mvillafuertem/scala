d253553d4ec8de457b79603f6d6142fd9f5758b3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-13 21:36:21 +0100

    chore(Dependencies) : update zio-kafka from 0.17.1 to 0.17.3 (#900)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -483,7 +483,7 @@ object Dependencies {
     val zioAkkaCluster            = "0.2.0"
     val zioInteropCats            = "3.2.9.0"
     val zioInteropReactiveStreams = "1.3.8"
-    val zioKafka                  = "0.17.1"
+    val zioKafka                  = "0.17.3"
   }
 
 }
