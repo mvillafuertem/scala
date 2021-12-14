343ced2dab5749410120663c76f451d505e0283b
Scala Steward <me@scala-steward.org>
2021-12-14 17:17:34 +0100

    chore(Dependencies) : update logback-classic from 1.2.7 to 1.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -465,7 +465,7 @@ object Dependencies {
     val jwtCirce                  = "4.3.0"
     val kafka                     = "2.8.0"
     val leveldbjniAll             = "1.8"
-    val logback                   = "1.2.7"
+    val logback                   = "1.2.8"
     val mongoScalaBson            = "4.4.0"
     val picocli                   = "4.6.1"
     val postgres                  = "42.3.1"
