167085b12dec73d941d97e272c0af4dca6e09379
Miguel Villafuerte <mivimur@hotmail.com>
2022-03-09 20:05:41 +0100

    chore(Dependencies) : update akka-stream-kafka, ... from 2.1.0 to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -448,7 +448,7 @@ object Dependencies {
     val akkaPersistenceCassandra  = "0.100"
     val akkaPersistenceInmemory   = "2.5.15.2"
     val akkaPersistenceJdbc       = "3.5.2"
-    val alpakkaKafka              = "2.1.0"
+    val alpakkaKafka              = "2.1.1"
     val alpakkaMongodb            = "3.0.4"
     val alpakkaSlick              = "3.0.4"
     val awsCdk                    = "1.147.0"
