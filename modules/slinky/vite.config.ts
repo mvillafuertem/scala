c3111e623088c3d7f5d5d6aa250dc802c5867efa
Miguel Villafuerte <mivimur@hotmail.com>
2022-05-01 12:31:22 +0200

    feature(slinky) : add dist

---
+++ modules/slinky/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+// https://vitejs.dev/config/
+export default defineConfig(({command, mode}) => {
+    const mainJS = `/target/scala-2.13/slinky-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
+    console.log('mainJS', mainJS)
+    const script = `<script type="module" src="${mainJS}"></script>`
+
+    return {
+        plugins: [
+            react(),
+            createHtmlPlugin({
+                minify: (process.env.NODE_ENV === 'production'),
+                inject: {data: {script}}
+            })],
+        build: {
+            outDir: '../../docs/slinky'
+        },
+        server: {
+            port: 8001,
+        }
+    }
+})
