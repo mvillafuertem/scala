299dbac3c8545d305bb96a25ca72669216195110
Scala Steward <me@scala-steward.org>
2022-05-12 21:29:08 +0200

    chore(Dependencies) : update ecs-patterns from 1.155.0 to 1.156.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -460,7 +460,7 @@ object Dependencies {
     val alpakkaKafka              = "3.0.0"
     val alpakkaMongodb            = "3.0.4"
     val alpakkaSlick              = "3.0.4"
-    val awsCdk                    = "1.155.0"
+    val awsCdk                    = "1.156.0"
     val awsLambda                 = "1.2.1"
     val awsSdk                    = "2.17.189"
     val cask                      = "0.7.11"
