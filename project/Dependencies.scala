e5f4a2ec67a741a31d6ac13089419bcf985d0b0d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-13 03:51:45 +0200

    chore(Dependencies) : update lambda from 2.17.189 to 2.17.190 (#1155)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -462,7 +462,7 @@ object Dependencies {
     val alpakkaSlick              = "3.0.4"
     val awsCdk                    = "1.156.0"
     val awsLambda                 = "1.2.1"
-    val awsSdk                    = "2.17.189"
+    val awsSdk                    = "2.17.190"
     val cask                      = "0.7.11"
     val cats                      = "2.7.0"
     val circe                     = "0.14.1"
