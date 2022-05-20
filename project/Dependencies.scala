d7301a177ed7080d68911c64686063dcd61d520a
Scala Steward <me@scala-steward.org>
2022-05-20 09:48:24 +0200

    chore(Dependencies) : update jslt from 0.1.11 to 0.1.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -472,7 +472,7 @@ object Dependencies {
     val ficus                     = "1.5.2"
     val h2                        = "2.1.212"
     val java8Compat               = "1.0.2"
-    val jslt                      = "0.1.11"
+    val jslt                      = "0.1.12"
     val jsoniter                  = "2.13.24"
     val jwtCirce                  = "4.3.0"
     val kafka                     = "3.2.0"
