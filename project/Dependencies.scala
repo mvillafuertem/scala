c42af068dba557dd5bafe3e430c51c4ad7e01523
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 19:48:24 +0000

    chore(Dependencies) : update zio-interop-cats from 3.2.9.1 to 3.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -493,7 +493,7 @@ object Dependencies {
     val testcontainersKafka       = "1.17.2"
     val zio                       = "1.0.13"
     val zioAkkaCluster            = "0.2.0"
-    val zioInteropCats            = "3.2.9.1"
+    val zioInteropCats            = "3.3.0"
     val zioInteropReactiveStreams = "2.0.0-RC4"
     val zioKafka                  = "0.17.5"
     val zioLoggingSlf4j           = "2.0.0-RC6"
