6912e30fe9545915825d3edc104c3eee3b1078f5
Scala Steward <scala_steward@virtuslab.com>
2022-07-17 15:18:22 +0000

    chore(Dependencies) : update cloudwatch, lambda, s3 from 2.17.232 to 2.17.233

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -462,7 +462,7 @@ object Dependencies {
     val alpakkaSlick              = "3.0.4"
     val awsCdk                    = "1.156.0"
     val awsLambda                 = "1.2.1"
-    val awsSdk                    = "2.17.232"
+    val awsSdk                    = "2.17.233"
     val cask                      = "0.7.11"
     val cats                      = "2.8.0"
     val circe                     = "0.14.1"
