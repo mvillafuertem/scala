75500ddfcd006cb19aff2cb5dd54b634dc94ae11
Scala Steward <scala_steward@virtuslab.com>
2022-07-21 15:37:33 +0000

    Revert commit(s) 36fc6eb

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -492,7 +492,7 @@ object Dependencies {
     val testcontainers            = "0.40.8"
     val testcontainersKafka       = "1.17.2"
     val zio                       = "1.0.15"
-    val zioAkkaCluster            = "0.3.0"
+    val zioAkkaCluster            = "0.2.0"
     val zioInteropCats            = "3.2.9.1"
     val zioInteropReactiveStreams = "2.0.0-RC4"
     val zioKafka                  = "0.17.5"
