29a3f44871e0dc1b9545951d28c4f6a96a76c6c8
Scala Steward <scala_steward@virtuslab.com>
2022-08-04 15:46:29 +0000

    chore(Dependencies) : update zio-config, zio-config-typesafe from 3.0.0-RC6 to 3.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -325,8 +325,8 @@ object Dependencies {
       Artifact.akkaStreamTyped
     ).map(_ % Version.akka) ++ Seq(
       // "org.iq80.leveldb" % "leveldb" % "0.12",
-      "dev.zio"                         %% "zio-config"          % "3.0.0-RC6",
-      "dev.zio"                         %% "zio-config-typesafe" % "3.0.0-RC6",
+      "dev.zio"                         %% "zio-config"          % "3.0.2",
+      "dev.zio"                         %% "zio-config-typesafe" % "3.0.2",
       Artifact.h2                        % Version.h2,
       Artifact.logback                   % Version.logback,
       Artifact.slick                     % Version.slick,
