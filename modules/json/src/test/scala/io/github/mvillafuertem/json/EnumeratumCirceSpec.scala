093f18102c0ec05367e92b8ab8700b95631e679d
Miguel Villafuerte <mivimur@hotmail.com>
2022-09-10 17:58:44 +0200

    feature(json) : add generic circe enum with name insensitive use case

--- modules/json/src/test/scala/io/github/mvillafuertem/json/EnumeratumCirceSpec.scala
+++ modules/json/src/test/scala/io/github/mvillafuertem/json/EnumeratumCirceSpec.scala
@@ -135,6 +135,43 @@ final class EnumeratumCirceSpec extends AnyFlatSpecLike with Matchers {
 
   }
 
+  it should "parse with custom decoder and name insensitive" in {
+
+    // g i v e n
+    sealed trait ShirtSize extends EnumEntry with UpperSnakecase
+
+    case object ShirtSize extends Enum[ShirtSize] with CirceEnum[ShirtSize] {
+
+      override implicit val circeDecoder: Decoder[ShirtSize] = new Decoder[ShirtSize] {
+        final def apply(c: HCursor): Result[ShirtSize] = implicitly[Decoder[String]].apply(c).flatMap { s =>
+          val maybeMember = withNameInsensitiveOption(s)
+          maybeMember match {
+            case Some(member) => Right(member)
+            case _ => Right(UnknownSize(s))
+          }
+        }
+      }
+
+      case object Small extends ShirtSize
+
+      case class UnknownSize(value: String) extends ShirtSize
+
+      val values = findValues
+
+    }
+
+    // w h e n
+    val small: ShirtSize = ShirtSize.Small
+
+    // t h e n
+    small.asJson shouldBe Json.fromString("SMALL")
+
+    Json.fromString("small").as[ShirtSize] shouldBe Right(small)
+    Json.fromString("Small").as[ShirtSize] shouldBe Right(small)
+    Json.fromString("SMALL").as[ShirtSize] shouldBe Right(small)
+
+  }
+
 }
 
 object EnumeratumCirceSpec {}
