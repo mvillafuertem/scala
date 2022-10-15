0b62cde6f6950837dcac6124b5ad1b6c1fe1b5f5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-15 05:01:47 +0200

    chore(Dependencies) : update scalatest from 3.2.13 to 3.2.14 (#1308)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -513,7 +513,7 @@ object Dependencies {
     val postgres                  = "42.4.1"
     val scalaCheck                = "3.2.2.0"
     val scalaJavaTime             = "2.0.0"
-    val scalaTest                 = "3.2.13"
+    val scalaTest                 = "3.2.14"
     val slick                     = "3.3.3"
     val slinky                    = "0.7.2"
     val spark                     = "3.3.0"
