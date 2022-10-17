1941c3ff275b21aadac947dd81a45f0a01ebf959
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 21:07:40 +0000

    chore(Dependencies) : update zio-akka-cluster from 0.2.0 to 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -522,7 +522,7 @@ object Dependencies {
     val testcontainers            = "0.40.10"
     val testcontainersKafka       = "1.17.3"
     val zio                       = "1.0.17"
-    val zioAkkaCluster            = "0.2.0"
+    val zioAkkaCluster            = "0.3.0"
     val zioInteropCats            = "3.2.9.1"
     val zioInteropReactiveStreams = "2.0.0-RC4"
     val zioKafka                  = "0.17.7"
