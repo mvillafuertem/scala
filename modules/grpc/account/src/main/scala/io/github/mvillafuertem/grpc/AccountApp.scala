c8cfa1c1f41aa8bdb25b39a66a7370a9cc3ae846
Miguel Villafuerte <mivimur@hotmail.com>
2023-01-05 22:49:47 +0100

    feature(grpc) : add neo4j repository

--- modules/grpc/account/src/main/scala/io/github/mvillafuertem/grpc/AccountApp.scala
+++ modules/grpc/account/src/main/scala/io/github/mvillafuertem/grpc/AccountApp.scala
@@ -1,11 +1,12 @@
 package io.github.mvillafuertem.grpc
 
 import cats.effect.{ IO, IOApp }
-import io.github.mvillafuertem.grpc.configuration.GreeterConfiguration
+import io.github.mvillafuertem.grpc.configuration.AccountConfiguration
 
 // sbt -mem 6000 "grpc-account/run"
-// docker-compose -f modules/grpc/greeter/src/main/resources/docker-compose.yml up
-object AccountApp extends IOApp.Simple with GreeterConfiguration {
+// docker-compose -f modules/grpc/account/src/main/resources/docker-compose.yml up grpcui
+// env UID=$(id -u) GID=$(id -g) docker-compose --file modules/grpc/account/src/main/resources/docker-compose.yml up neo4j
+object AccountApp extends IOApp.Simple with AccountConfiguration {
 
   override def run: IO[Unit] = resource
     .evalMap(server => IO.delay(server.start()))
