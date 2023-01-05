fd3abfc10f789734eeef7069a70c06b5e38c2cf2
Scala Steward <scala_steward@virtuslab.com>
2023-01-05 17:42:02 +0000

    Revert commit(s) 1bf54eff

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -244,10 +244,10 @@ object Dependencies {
     Artifact.picocliCodegen % Version.picocli % Provided
   )
 
-  private val Protobuf              = Configurations.config("protobuf")
+  private val Protobuf = Configurations.config("protobuf")
   val `grpc-greeter`: Seq[ModuleID] = Seq(
     // G R P C
-    "com.google.api.grpc"   % "grpc-google-common-protos" % "2.7.4"                                 % Protobuf,
+    "com.google.api.grpc"   % "grpc-google-common-protos" % "2.7.1"                                 % Protobuf,
     "org.openapitools"      % "openapi-generator"         % "5.0.1",
     // (optional) If you need scalapb/scalapb.proto or anything from
     // google/protobuf/*.proto
