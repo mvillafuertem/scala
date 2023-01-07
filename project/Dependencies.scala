aceb68e45d337a20b163a599c11b35c415cfb673
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 21:11:23 +0000

    chore(Dependencies) : update grpc-google-common-protos from 2.7.1 to 2.7.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -528,7 +528,7 @@ object Dependencies {
     val dijon                     = "0.6.0"
     val enumeratumCirce           = "1.7.0"
     val ficus                     = "1.5.2"
-    val grpcGoogleCommonProtos    = "2.7.1"
+    val grpcGoogleCommonProtos    = "2.7.4"
     val h2                        = "2.1.214"
     val http4s                    = "1.0.0-M35"
     val java8Compat               = "1.0.2"
