763dbb896d9be28a3afda4c4709ad20c1583fb83
Scala Steward <scala_steward@virtuslab.com>
2023-01-13 16:36:05 +0000

    chore(Dependencies) : update mongo-scala-bson from 4.4.2 to 4.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -538,7 +538,7 @@ object Dependencies {
     val kafka                     = "3.3.1"
     val leveldbjniAll             = "1.8"
     val logback                   = "1.2.11"
-    val mongoScalaBson            = "4.4.2"
+    val mongoScalaBson            = "4.8.2"
     val neotypes                  = "0.23.1"
     val openapiGenerator          = "5.0.1"
     val picocli                   = "4.6.1"
