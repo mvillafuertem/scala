4f1c0be4c789f04567fac301b238cd1adc6a12c7
Scala Steward <scala_steward@virtuslab.com>
2023-01-15 16:17:59 +0000

    Revert commit(s) c8114d00

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -498,7 +498,7 @@ object Dependencies {
     val cask                      = "0.7.11"
     val cats                      = "2.9.0"
     val catsEffect                = "3.4.2"
-    val circe                     = "0.14.3"
+    val circe                     = "0.14.1"
     val http4s                    = "1.0.0-M35"
     val curator                   = "5.3.0"
     val dijon                     = "0.6.0"
