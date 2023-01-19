a2ed6d0f78a21507c4fe403478bed564b36ba009
Scala Steward <scala_steward@virtuslab.com>
2023-01-19 19:45:23 +0000

    chore(Dependencies) : update kafka from 3.3.1 to 3.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -535,7 +535,7 @@ object Dependencies {
     val jslt                      = "0.1.13"
     val jsoniter                  = "2.17.5"
     val jwtCirce                  = "4.3.0"
-    val kafka                     = "3.3.1"
+    val kafka                     = "3.3.2"
     val leveldbjniAll             = "1.8"
     val logback                   = "1.2.11"
     val mongoScalaBson            = "4.4.2"
