57ff7dec4da9366d42b00f68ef6bd3ad6ac91c4c
Miguel Villafuerte <mivimur@hotmail.com>
2023-03-18 16:10:24 +0100

    feature(account) : add example with type members

--- modules/grpc/account/src/main/scala/io/github/mvillafuertem/grpc/domain/application/UseCase.scala
+++ modules/grpc/account/src/main/scala/io/github/mvillafuertem/grpc/domain/application/UseCase.scala
@@ -1,8 +1,50 @@
 package io.github.mvillafuertem.grpc.domain.application
 
+import cats.Applicative
+import cats.data.Validated
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.catsSyntaxApplicativeId
+import io.github.mvillafuertem.grpc.domain.error.AccountError
 import io.github.mvillafuertem.grpc.domain.model.Validation
-import io.github.mvillafuertem.grpc.greeter.{ HelloReply, HelloRequest }
+import io.github.mvillafuertem.grpc.greeter.{HelloReply, HelloRequest}
 import io.grpc.Metadata
 
 // type UseCase[F[_], A <: HelloRequest, R <: HelloReply] = ((A, Metadata) => F[Validation[R]])
 trait UseCase[F[_], A <: HelloRequest, R <: HelloReply] extends ((A, Metadata) => F[Validation[R]])
+
+object TestType extends IOApp {
+
+  def apply[F[_]: Applicative]: UseCase[F, HelloRequest, HelloReply] = new UseCase[F, HelloRequest, HelloReply] {
+    override def apply(request: HelloRequest, metadata: Metadata): F[Validation[HelloReply]] =
+      Validated
+        .cond(request.name.length > 2, HelloReply(request.name), AccountError.BankIsInvalid)
+        .toValidatedNel[AccountError, HelloReply]
+        .pure[F]
+  }
+  trait MyUseCase {
+    type Request
+    type Response
+    type Monad[_]
+    def apply(request: Request, metadata: Metadata): Monad[Response]
+  }
+
+
+  def validate[X[_]: Applicative] = new MyUseCase {
+    type Request = String
+
+    type Response = String
+
+    type Monad[_] = Option[_]
+
+    override def apply(request: String, metadata: Metadata): Monad[Response] = Applicative.apply[Option].pure("")
+
+  }
+
+
+  type MT[A] = MyUseCase { type Monad = MT[A] }
+  def validate[F[_]]: String = ???
+
+
+
+  override def run(args: List[String]): IO[ExitCode] = IO().as(ExitCode.Success)
+}
---
+++ modules/grpc/account/src/main/scala/io/github/mvillafuertem/grpc/domain/application/UseCaseTypeMembers.scala
@@ -0,0 +1,39 @@
+package io.github.mvillafuertem.grpc.domain.application
+
+import io.grpc.Metadata
+
+trait UseCaseTypeMembers {
+
+  type Request
+  type Response
+  type Monad[_]
+
+  def apply(request: Request, metadata: Metadata): Monad[Response]
+
+}
+
+object UseCaseTypeMembers {
+
+
+  type AuX[_] = UseCaseTypeMembers { type Monad[_] = AuX[_] }
+
+  def apply[F[_]: AuX]: AuX[F] = implicitly[AuX[F]]
+
+
+  implicit val a = new AuX[Option[_]] { self =>
+    override type Request = String
+    override type Response = String
+    override type Monad[Response] = Option[Response]
+
+     def apply(request: Request, metadata: Metadata): Monad[Response] = Option("")
+
+  }
+
+  // def useCase[F[_]: AuX] = UseCaseTypeMembers[F].apply("", new Metadata())
+
+  private val value: Option[String] = a.apply("", new Metadata())
+
+  println(value)
+
+
+}
