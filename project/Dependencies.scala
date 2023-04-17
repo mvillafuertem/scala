0e710c7e2da31de96e8b33b512fe037f5a9132a0
Scala Steward <scala_steward@virtuslab.com>
2023-04-17 17:31:40 +0000

    chore(Dependencies) : update cats-effect from 3.4.2 to 3.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -522,7 +522,7 @@ object Dependencies {
     val awsSdk                    = "2.17.293"
     val cask                      = "0.7.11"
     val cats                      = "2.9.0"
-    val catsEffect                = "3.4.2"
+    val catsEffect                = "3.4.9"
     val circe                     = "0.14.1"
     val curator                   = "5.3.0"
     val dijon                     = "0.6.0"
