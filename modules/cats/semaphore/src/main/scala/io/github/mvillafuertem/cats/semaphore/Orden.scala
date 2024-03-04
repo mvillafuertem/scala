e0d766fc87c4bc60a71047a26365edc6c29402a5
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2024-03-04 20:51:36 +0100

    feature(cats) : add cats stm with txn

--- modules/cats/semaphore/src/main/scala/io/github/mvillafuertem/cats/semaphore/Orden.scala
+++ modules/cats/semaphore/src/main/scala/io/github/mvillafuertem/cats/semaphore/Orden.scala
@@ -73,3 +73,43 @@ class Orden(id: String, c: Char, sem: java.util.concurrent.Semaphore) extends Th
   }
 }
  */
+
+
+
+/*def apply[F[_], S <: STM[F]](db: S#TVar[List[Car]]): CarRepository[S#Txn] =
+  new CarRepository[S#Txn] {
+    override def findByAvailableSeats(availableSeats: Int): S#Txn[List[Car]] = db.get
+
+    override def deleteAll(): S#Txn[Unit] = db.modify(_ => List.empty)
+
+    override def deleteById(id: Int): S#Txn[Unit] = db.modify(_.filterNot(_.id.value == id))
+
+    override def findById(id: Int): S#Txn[Option[Car]] = db.get.map(_.find(_.id.value == id))
+
+    override def getAll: S#Txn[List[Car]] = db.get
+
+    override def insert(value: Car): S#Txn[Car] = db.modify(_ :+ value).as(value)
+
+    override def insertAll(value: Car*): S#Txn[List[Car]] = ???
+
+    override def update(a: Car): S#Txn[Car] = db.modify(_.filterNot(_.id.value == a.id.value) :+ a).as(a)
+  }
+
+val actual: Seq[Car] = (for {
+
+  stm <- STM.runtime[IO]
+  db  <- stm.commit(stm.TVar.of(List(Car(CarId(1), Seat(4), Seat(4)))))
+  repository = InMemoryCarRepository[IO, stm.type](db)
+  txn <- stm
+    .commit {
+      for {
+        _    <- repository.insert(Car(CarId(0), Seat(4), Seat(4)))
+        _    <- stm.raiseError(new RuntimeException("error provocado"))
+        cars <- repository.getAll
+      } yield cars
+    }
+    .handleErrorWith(_ => stm.commit(repository.getAll))
+
+} yield txn).unsafeRunSync()
+
+actual shouldBe List(Car(CarId(1), Seat(4), Seat(4)))*/
